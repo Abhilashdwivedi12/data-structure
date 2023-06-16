fc1ea86b284e78733c02ebd394b1e8aa469321ba
Abhilash Dwivedi <107290844+Abhilashdwivedi12@users.noreply.github.com>
2023-06-16 23:59:24 +0530

    Update implementation of node.cpp
    implementation of node .it is no taken by the user

--- implementation of node.cpp
+++ implementation of node.cpp
@@ -1,21 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 struct node{
 	int data;
 	struct node *next;
 };
-
-	struct node *head,*newnode,*temp;
+int main()
+{
+	struct node *head,*temp,*newnode;
 	head=0;
-	newnode=(struct node*)malloc(sizeof(struct node));
-	printf("enter data");
-	scanf("%d",&newnode->data);
-	newnode->next=0;
-	if (head==0){
-		head==newnode;
-		
-	}else{
-		head->next=newnode;
+	int length;
+	printf("enter the no of node you want to :");
+	scanf("%d",&length);
+	for(int i=0;i<length;i++)
+	{
+		newnode=(struct node*)malloc(sizeof(struct node));
+		printf("enter the data %d:",i+1);
+		scanf("%d",&newnode->data);
+		if(head==0)
+		{
+			head=temp=newnode;
+		}
+		else{
+			temp->next=newnode;
+			temp=newnode;
+			
+		}
 	}
+	temp=head;
+	printf("linked list");
+	while(temp!=0){
+		printf("  %d",temp->data);
+		temp=temp->next;
+	}
+	printf("\n");
+}
 	
 
